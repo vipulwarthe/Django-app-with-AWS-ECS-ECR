f3db576057f0a0f7a87b5a95abff20992434d49a
Vipulkumar Warthe <126955342+vipulwarthe@users.noreply.github.com>
2025-11-02 19:13:46 +0530

    Update views.py

--- basic_django_app/views.py
+++ basic_django_app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html", {"message": "Hi, My name is Kevin Joe Harris"})
+    return render(request, "home.html", {"message": "Hi, My name is Vipul Warthe"})
